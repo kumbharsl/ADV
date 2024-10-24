3aec73678c1282c94e6ad11487c4dd2113a8d3d1
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-10-24 11:21:52 +0530

    update

---
+++ books/lib/pages.dart
@@ -0,0 +1,235 @@
+// Example of Page 1
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 1')),
+      body: const Center(child: Text('This is Page 1')),
+    );
+  }
+}
+
+// Example of Page 2
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 2')),
+      body: const Center(child: Text('This is Page 2')),
+    );
+  }
+}
+
+// Example of Page 3
+class Page3 extends StatelessWidget {
+  const Page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 3')),
+      body: const Center(child: Text('This is Page 3')),
+    );
+  }
+}
+
+// Example of Page 4
+class Page4 extends StatelessWidget {
+  const Page4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 4')),
+      body: const Center(child: Text('This is Page 4')),
+    );
+  }
+}
+
+// Example of Page 5
+class Page5 extends StatelessWidget {
+  const Page5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 5')),
+      body: const Center(child: Text('This is Page 5')),
+    );
+  }
+}
+
+// Example of Page 6
+class Page6 extends StatelessWidget {
+  const Page6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 6')),
+      body: const Center(child: Text('This is Page 6')),
+    );
+  }
+}
+
+// Example of Page 7
+class Page7 extends StatelessWidget {
+  const Page7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 7')),
+      body: const Center(child: Text('This is Page 7')),
+    );
+  }
+}
+
+// Example of Page 8
+class Page8 extends StatelessWidget {
+  const Page8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 8')),
+      body: const Center(child: Text('This is Page 8')),
+    );
+  }
+}
+
+// Example of Page 9
+class Page9 extends StatelessWidget {
+  const Page9({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 9')),
+      body: const Center(child: Text('This is Page 9')),
+    );
+  }
+}
+
+// Example of Page 10
+class Page10 extends StatelessWidget {
+  const Page10({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 10')),
+      body: const Center(child: Text('This is Page 10')),
+    );
+  }
+}
+
+// Example of Page 11
+class Page11 extends StatelessWidget {
+  const Page11({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 11')),
+      body: const Center(child: Text('This is Page 11')),
+    );
+  }
+}
+
+// Example of Page 12
+class Page12 extends StatelessWidget {
+  const Page12({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 12')),
+      body: const Center(child: Text('This is Page 12')),
+    );
+  }
+}
+
+// Example of Page 13
+class Page13 extends StatelessWidget {
+  const Page13({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 13')),
+      body: const Center(child: Text('This is Page 13')),
+    );
+  }
+}
+
+// Example of Page 14
+class Page14 extends StatelessWidget {
+  const Page14({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 14')),
+      body: const Center(child: Text('This is Page 14')),
+    );
+  }
+}
+
+// Example of Page 15
+class Page15 extends StatelessWidget {
+  const Page15({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 15')),
+      body: const Center(child: Text('This is Page 15')),
+    );
+  }
+}
+
+// Example of Page 16
+class Page16 extends StatelessWidget {
+  const Page16({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 16')),
+      body: const Center(child: Text('This is Page 16')),
+    );
+  }
+}
+
+// Example of Page 17
+class Page17 extends StatelessWidget {
+  const Page17({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 17')),
+      body: const Center(child: Text('This is Page 17')),
+    );
+  }
+}
+
+// Example of Page 18
+class Page18 extends StatelessWidget {
+  const Page18({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 18')),
+      body: const Center(child: Text('This is Page 18')),
+    );
+  }
+}
